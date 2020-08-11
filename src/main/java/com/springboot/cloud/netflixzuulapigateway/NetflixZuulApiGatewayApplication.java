59ccd0d41aff462912301640a266d92df61ef451
Alexandre Halas <alexandrehalas@gmail.com>
2020-08-11 17:39:27 -0300

    Added Spring Cloud Sleuth

--- src/main/java/com/springboot/cloud/netflixzuulapigateway/NetflixZuulApiGatewayApplication.java
+++ src/main/java/com/springboot/cloud/netflixzuulapigateway/NetflixZuulApiGatewayApplication.java
@@ -4,6 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+
+import brave.sampler.Sampler;
 
 @EnableZuulProxy
 @EnableDiscoveryClient
@@ -14,4 +17,10 @@ public class NetflixZuulApiGatewayApplication {
 		SpringApplication.run(NetflixZuulApiGatewayApplication.class, args);
 	}
 
+	//Added Spring Cloud Sleuth to the Netflix Zuul API Gateway
+	@Bean
+	public Sampler defaultSampler() {
+		return Sampler.ALWAYS_SAMPLE;
+	}
+	
 }
